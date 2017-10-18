e331e63f8ad5b7f311466e5e76b739bb77d14a52
Yannis Lang <jopolaz@hotmail.fr>
2017-10-18 12:04:45 +0200

    CardName get Value

--- src/main/java/CardName.java
+++ src/main/java/CardName.java
@@ -12,4 +12,43 @@ public enum CardName{
     public String toString() {
         return super.toString();
     }
+
+    public int getValue(boolean isAtout)
+    {
+        switch (this.toString().toLowerCase())
+        {
+            case "as":
+                return 11;
+
+            case "sept": case "huit":
+                if (isAtout)
+                    return 0;
+
+            case "neuf":
+                if (isAtout)
+                    return 14;
+                else
+                    return 0;
+
+            case "dix":
+                return 10;
+
+            case "valet":
+                if (isAtout)
+                    return 20;
+                else
+                    return 2;
+
+            case "dame":
+                return 3;
+
+            case "roi":
+              return 4;
+
+            default:
+                return 0;
+
+
+        }
+    }
 }
